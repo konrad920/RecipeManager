2a5c19f0ce6fcb5b61e1d12eebbfb81a1a08de1c
konrad920 <128093250+konrad920@users.noreply.github.com>
2024-11-24 11:12:51 +0100

    Create Main Frame

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.awt.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main extends JFrame {
+
+    Main() {
+        /*
+        create frame manually in constructor but we can extend class JFrame by class Main() and use methods from the class JFrame
+        JFrame frame = new JFrame();
+        frame.setTitle("Recipe Manager");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(300,100);
+//        frame.setSize(new Dimension(100, 100));
+        frame.setLocation(100, 200);
+//        frame.setLocation(new Point(100, 200));
+        frame.setResizable(true);
+         */
+
+        this.setBounds(100, 200, 300, 100);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setIconImage(getToolkit().getImage("img\\recipes.png"));
+        this.setTitle("Recipe Manager");
+    }
+    public static void main(String[] args) {
+        new Main();
     }
 }
